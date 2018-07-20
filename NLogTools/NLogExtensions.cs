f5ab7eb53190f04cbf921e776ffb03b6321a3408
Roman Pokrovskij <roman.pokrovskij@gmail.com>
2018-07-20 13:40:34 +0300

    initial

---
+++ NLogTools/NLogExtensions.cs
@@ -0,0 +1,14 @@
+﻿using NLog.Conditions;
+
+namespace DashboardCode.NLogTools
+{
+    [ConditionMethods]
+    public static class NLogExtensions
+    {
+        readonly static PerDayСounter PerDayСounter = new PerDayСounter();
+
+        [ConditionMethod("daycount")]
+        public static long Count() =>
+            PerDayСounter.Count();
+    }
+}
---
+++ nugetall.ps1
@@ -0,0 +1,8 @@
+$SolutionFolderPath = $PSScriptRoot 
+cd $SolutionFolderPath
+
+$sign = Read-Host 'Enter sign'
+
+#cd .\NLogTools\bin\Release
+#nuget push DashboardCode.NLogTools.1.0.0.nupkg $sign -Source https://api.nuget.org/v3/index.json
+
