b08690800e41983a36aa38a59263534e65b22fc3
Roman Pokrovskij <roman.pokrovskij@gmail.com>
2018-08-10 18:24:00 +0300

    update NLOG

--- nugetall.ps1
+++ nugetall.ps1
@@ -3,6 +3,6 @@ cd $SolutionFolderPath
 
 $sign = Read-Host 'Enter sign'
 
-#cd .\NLogTools\bin\Release
-#nuget push DashboardCode.NLogTools.1.0.0.nupkg $sign -Source https://api.nuget.org/v3/index.json
+cd .\NLogTools\bin\Release
+nuget push DashboardCode.NLogTools.1.0.1.nupkg $sign -Source https://api.nuget.org/v3/index.json
 
