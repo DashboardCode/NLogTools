af7b9f9767a922c86baea6e7e4b302b5dbe401e6
Roman Pokrovskij <roman.pokrovskij@gmail.com>
2019-04-14 16:43:32 +0300

    rfr update

--- nugetall.ps1
+++ nugetall.ps1
@@ -4,5 +4,5 @@ cd $SolutionFolderPath
 $sign = Read-Host 'Enter sign'
 
 cd .\NLogTools\bin\Release
-nuget push DashboardCode.NLogTools.1.0.7.nupkg $sign -Source https://api.nuget.org/v3/index.json
+nuget push DashboardCode.NLogTools.1.0.9.nupkg $sign -Source https://api.nuget.org/v3/index.json
 
