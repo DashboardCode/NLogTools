9aa71b96c1c27ee36a8d818031c14d9c7d0455a3
Roman Pokrovskij <roman.pokrovskij@gmail.com>
2019-06-29 20:26:33 +0300

    btf

--- nugetall.ps1
+++ nugetall.ps1
@@ -1,7 +1,7 @@
 $SolutionFolderPath = $PSScriptRoot 
 cd $SolutionFolderPath
 
-$sign = Read-Host 'Enter sign'
+$sign = Read-Host 'Enter nuget sign'
 
 cd .\NLogTools\bin\Release
 nuget push DashboardCode.NLogTools.1.0.11.nupkg $sign -Source https://api.nuget.org/v3/index.json
