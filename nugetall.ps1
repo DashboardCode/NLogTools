726b6ca054052205ac13eb37b29c2d47757afaf8
Roman Pokrovskij <roman.pokrovskij@gmail.com>
2019-09-17 20:52:56 +0300

    updates

--- nugetall.ps1
+++ nugetall.ps1
@@ -4,5 +4,5 @@ cd $SolutionFolderPath
 $sign = Read-Host 'Enter nuget sign'
 
 cd .\NLogTools\bin\Release
-nuget push DashboardCode.NLogTools.1.0.11.nupkg $sign -Source https://api.nuget.org/v3/index.json
+nuget push DashboardCode.NLogTools.1.0.12.nupkg $sign -Source https://api.nuget.org/v3/index.json
 
