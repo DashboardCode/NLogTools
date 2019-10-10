d7073d4e124f0fbf20d2fa600cc9adfece5059ec
Roman Pokrovskij <roman.pokrovskij@gmail.com>
2019-10-10 21:49:56 +0300

    migr to netcore3

--- nugetall.ps1
+++ nugetall.ps1
@@ -4,5 +4,5 @@ cd $SolutionFolderPath
 $sign = Read-Host 'Enter nuget sign'
 
 cd .\NLogTools\bin\Release
-nuget push DashboardCode.NLogTools.1.0.12.nupkg $sign -Source https://api.nuget.org/v3/index.json
+nuget push DashboardCode.NLogTools.3.0.0.nupkg $sign -Source https://api.nuget.org/v3/index.json
 
