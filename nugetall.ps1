fad2443e7d331edfd0da33b771d69a5199069cf9
Roman Pokrovskij <roman.pokrovskij@gmail.com>
2020-07-12 03:24:31 +0300

    migrations

--- nugetall.ps1
+++ nugetall.ps1
@@ -4,5 +4,5 @@ cd $SolutionFolderPath
 $sign = Read-Host 'Enter nuget sign'
 
 cd .\NLogTools\bin\Release
-nuget push DashboardCode.NLogTools.3.0.1.nupkg $sign -Source https://api.nuget.org/v3/index.json
+nuget push DashboardCode.NLogTools.3.0.2.nupkg $sign -Source https://api.nuget.org/v3/index.json
 
